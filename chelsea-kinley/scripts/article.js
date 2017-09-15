312e9c359e0df986e3075359aee6ac4f56d29891
Kinley Ramson <kinleyramson@gmail.com>
2017-09-14 17:03:51 -0700

    finished todo 3, compiling and rendering the template with handlebars

--- chelsea-kinley/scripts/article.js
+++ chelsea-kinley/scripts/article.js
@@ -12,9 +12,12 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // DONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
+
+  var source = $('#post-template').html();
+  var template = Handlebars.compile(source);
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
